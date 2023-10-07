240a00899a0b091a436d853da2eec9b24448d443
SomeOne768 <jalil0169@gmail.com>
2023-10-07 10:19:12 +0200

    adding coplien methods

--- tp_1/src/Vecteur.cpp
+++ tp_1/src/Vecteur.cpp
@@ -1,1 +1,54 @@
 #include "Vecteur.hpp"
+
+template <typename T>
+Vecteur<T>::Vecteur()
+{
+    tab = nullptr;
+    try
+    {
+        _taille = 10;
+        tab = new T[_taille];
+    }
+    catch (std::bad_alloc &e)
+    {
+        _taille = 0;
+        std::cerr << e.what();
+    }
+}
+
+template <typename T>
+Vecteur<T>::~Vecteur()
+{
+    if (tab)
+    {
+        delete[] tab;
+    }
+}
+
+template <typename T>
+Vecteur<T>::Vecteur(const Vecteur<T> &v)
+{
+    tab = new T[v._taille];
+    for (unsigned int i = 0; i < v._taille; i++)
+    {
+        tab[i] = v.tab[i];
+    }
+    _taille = v._taille;
+}
+
+template <typename T>
+Vecteur<T>& Vecteur<T>::operator=(const Vecteur<T> &v)
+{
+    if (this != &v)
+    {
+        this.~Vecteur<T>();
+        tab = new T[v._taille];
+        _taille = v._taille;
+        for (unsigned int i = 0; i < v._taille; i++)
+        {
+            tab[i] = v.tab[i];
+        }
+    }
+
+    return *this;
+}
--- tp_1/src/Vecteur.hpp
+++ tp_1/src/Vecteur.hpp
@@ -1,8 +1,17 @@
 #ifndef VECTEUR_AZ_2023ZZ3
 #define VECTEUR_AZ_2023ZZ3
+#include <iostream>
 
+template <typename T>
 class Vecteur{
 
+    T *tab;
+    unsigned int _taille;
+public:
+    Vecteur<T>();
+    ~Vecteur<T>();
+    Vecteur<T>(const Vecteur<T>&);
+    Vecteur<T>& operator=(const Vecteur<T>&);
 };
 
 #endif
