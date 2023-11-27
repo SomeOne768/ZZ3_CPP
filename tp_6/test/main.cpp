45eafeecfdddfe6fdf103fcdc452505efbeef0b4
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-11-27 13:58:09 +0100

    Cours exemples

--- tp_6/test/main.cpp
+++ tp_6/test/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <thread>
 
+void zzz(unsigned n)
+{
+    std::cout << "[" << n << "] zzz..."
+              << "\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
 
 int main()
 {
+
+    // Exemple 1
+    std::thread t[4];
+
+    for (int i = 0; i < 4; i++)
+        t[i] = std::thread(zzz, i);
+
+    for (int i = 0; i < 4; i++)
+        t[i].join();
+
+    // Exemple 2
+    std::thread t2[4];
+    for (int i = 0; i < 4; i++)
+        t2[i] = std::thread([](unsigned n)
+                            {
+                                std::cout << "[" << n << "] zzz..." << "\n";
+                                std::this_thread::sleep_for(std::chrono::seconds(1)); 
+                            }, i);
+    for (int i = 0; i < 4; i++)
+        t2[i].join();
+
+    // Exemple 3
+    std::thread t3[4];
+    for (int i = 0; i < 4; i++)
+        t3[i] = std::thread([=]()
+                            {
+                                std::cout << "[" << i << "] zzz..." << "\n";
+                                std::this_thread::sleep_for(std::chrono::seconds(1)); 
+                            });
+    for (int i = 0; i < 4; i++)
+        t3[i].join();
 
     return 0;
 }
