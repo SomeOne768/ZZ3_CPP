ad1eee3d91592fc315fa52815b6e821251ab498d
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-11-27 14:07:42 +0100

    adding mutex

--- tp_6/test/main.cpp
+++ tp_6/test/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
+std::mutex mutex;
 void zzz(unsigned n)
 {
+    mutex.lock();
     std::cout << "[" << n << "] zzz..."
               << "\n";
+
+    mutex.unlock();
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
@@ -23,11 +28,11 @@ int main()
     // Exemple 2
     std::thread t2[4];
     for (int i = 0; i < 4; i++)
-        t2[i] = std::thread([](unsigned n)
-                            {
-                                std::cout << "[" << n << "] zzz..." << "\n";
-                                std::this_thread::sleep_for(std::chrono::seconds(1)); 
-                            }, i);
+        t2[i] = std::thread([](unsigned n){
+                                {std::lock_guard<std::mutex> verrou(mutex);
+                                std::cout << "[" << n << "] zzz..." << "\n";}
+                                std::this_thread::sleep_for(std::chrono::seconds(1)); },
+                            i);
     for (int i = 0; i < 4; i++)
         t2[i].join();
 
@@ -36,9 +41,9 @@ int main()
     for (int i = 0; i < 4; i++)
         t3[i] = std::thread([=]()
                             {
-                                std::cout << "[" << i << "] zzz..." << "\n";
-                                std::this_thread::sleep_for(std::chrono::seconds(1)); 
-                            });
+                                {std::lock_guard<std::mutex> verrou(mutex);
+                                std::cout << "[" << i << "] zzz..." << "\n";}
+                                std::this_thread::sleep_for(std::chrono::seconds(1)); });
     for (int i = 0; i < 4; i++)
         t3[i].join();
 
